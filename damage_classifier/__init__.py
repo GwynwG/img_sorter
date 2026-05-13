f766f18f3c22df223483e9bd9e480e00cf420e24
GwynwG <38907439+GwynwG@users.noreply.github.com>
2026-05-13 23:32:13 +0800

    Resolve merge conflicts in damage classifier

--- damage_classifier/__init__.py
+++ damage_classifier/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 ﻿"""Simulated damage spot classification package."""
-=======
-"""Simulated damage spot classification package."""
->>>>>>> 6ed2eb1deaef4a6128758f9b78052bf38af78ac2
 
 __all__ = ["DamagePrediction", "DamagePredictionPipeline"]
